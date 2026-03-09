1ac9ba2c3b142f5dcc672416b4ca4e538893dd86
kashapalinin <78311696+kashapalinin@users.noreply.github.com>
2026-03-09 15:36:31 +0300

    init

---
+++ SUIRAExample/SUIRAExample/SUIRAExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIRAExampleApp.swift
+//  SUIRAExample
+//
+//  Created by Павел Калинин on 09.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct SUIRAExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
